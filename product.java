3ec33a3092e1b8ff9f2cdb461f17988477dd3014
shrutitapre <73237627+shrutitapre@users.noreply.github.com>
2020-10-21 19:34:27 +0530

    online shopping

---
+++ product.java
@@ -0,0 +1,39 @@
+
+
+public class product {
+private int productId;
+private String productName;
+private String desc;
+private String price;
+public int getproductId() {
+	return productId;
+}
+public void setproduct(int productId) {
+	this.productId = productId;
+}
+public String getproductName() {
+	return productName;
+}
+public void setproductName(String productName) {
+	this.productName = productName;
+}
+public String getDesc() {
+	return desc;
+}
+public void setDesc(String desc) {
+	this.desc = desc;
+}
+public double getprice() {
+	return price;
+}
+public void setprice(double price) {
+	this.price = price;
+}
+
+
+
+	
+
+	}
+
+}
